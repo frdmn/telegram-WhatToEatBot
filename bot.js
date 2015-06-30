a58531694a13c91168b279b508cae5fe130bd045
Jonas Friedmann <j@frd.mn>
2015-06-30 14:19:25 +0200

    It's working 😍

--- bot.js
+++ bot.js
@@ -1,10 +1,48 @@
+/* Require modules */
+
 var nodeTelegramBot = require('node-telegram-bot')
-    , config = require('./config.json');
+    , config = require('./config.json')
+    , request = require("request")
+    , cheerio = require("cheerio");
+
+/* Functions */
+
+/**
+ * Uses the request module to return the body of a web page
+ * @param  {string}   url
+ * @param  {callback} callback
+ * @return {string}
+ */
+
+function getWebContent(url, callback){
+  request({
+    uri: url,
+  }, function(error, response, body) {
+    callback(body);
+  });
+}
+
+/* Logic */
+
+var recipeURL = 'http://www.cookstr.com/searches/surprise';
 
 var bot = new nodeTelegramBot({
   token: config.token
 })
 .on('message', function (message) {
-  console.log(message);
+  /* Process "/random" command */
+  if (message.text == "/dinner") {
+    getWebContent(recipeURL, function(data){
+      // Parse DOM and recipe informations
+      var $ = cheerio.load(data)
+          , recipeName = $("span[itemprop=name]").html()
+          , recipeURL = $("meta[property='og:url']").attr('content');
+      // Send bot reply
+      bot.sendMessage({
+          chat_id: message.chat.id,
+          text: 'What about "' + recipeName + '"? ' + recipeURL
+      });
+    });
+  }
 })
 .start();
